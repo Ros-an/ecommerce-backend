192d6861ef376a4e644283abcaf8bad7672654c8
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-30 22:15:49 +0530

    connect sever to DB using Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+// app
+const app = express();
+
+// DB
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("this is eror", err));
+
+// routes
 app.get("/", (req, res) => {
   res.send("Hello Roshan! kya hal chal");
 });
