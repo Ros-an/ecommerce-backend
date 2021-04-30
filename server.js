6bbcb5556a76d1f291608f2bb3799e5e50e510a6
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-30 15:39:47 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Hello Roshan! kya hal chal");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
