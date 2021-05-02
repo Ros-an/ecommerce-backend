e78f9fb4e3f300e5135471d6484cf1ebcc2130bf
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-02 11:40:41 +0530

    renamed

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// importing logic(method) from controller
-const { signup } = require("../controllers/user");
-
-router.post("/signup", signup);
-
-module.exports = router;
